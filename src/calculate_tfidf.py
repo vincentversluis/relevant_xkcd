3b9e05b0ab9eff7255ff22caf3ff9f576ebedb8f
vincentversluis <vincentversluis@hotmail.com>
2025-08-09 15:31:16 +0200

    Put the tfidf calculations in a loop per ngram length

--- src/calculate_tfidf.py
+++ src/calculate_tfidf.py
@@ -9,6 +9,7 @@
 # TODO: Consider a separate calculation for each length of ngram
 # TODO: Strip upper case using NER
 # TODO: Build in extra step of removing punctuation and stopwords
+# TODO: Docstrings
 
 # %% IMPORTS
 from nltk.corpus import stopwords
@@ -23,6 +24,7 @@ from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from itertools import chain
+from tqdm import tqdm
 
 # %% INPUTS
 db_path = '../data/relevant_xkcd.db'
@@ -40,10 +42,6 @@ lemmatizer = WordNetLemmatizer()
 stopwords_pattern = r'\b(?:' + '|'.join(map(re.escape, stopwords.words('english'))) + r')\b'
 punctuation_pattern = '|'.join(map(re.escape, string.punctuation))
 split_pattern = f'({stopwords_pattern})|({punctuation_pattern})'
-
-# %% GET DATA
-xkcd_properties_df = db_utils.get_xkcd_properties(db_path)
-xkcd_explanations_df = db_utils.get_xkcd_explained(db_path)
 
 # %% FUNCTIONS
 def split_text_by_regex(text: str, split_pattern: str) -> list:
@@ -133,70 +131,70 @@ def get_preprocessed_tokens(text: str, n_gram_length: int=1) -> list:
     tokens = list(chain(*[get_ngrams(fragment, n=n_gram_length) for fragment in text_split]))
     return tokens
 
-text = "You are Creating a cocktail kings menu partying stockrace for a PARTY pooper in New York City, which has a party place where party people party"
-for n_gram_length in range(1, n_gram_max_length+1):
-    print(f"n-gram length {n_gram_length}:")
-    tokens = get_preprocessed_tokens(text, n_gram_length=n_gram_length)
-    print(tokens)
-    print()
-
-# %%
-
-def get_tfidf(df: pd.DataFrame, doc_id_col: str, text_col: str) -> pd.DataFrame:
+def get_tfidf(df: pd.DataFrame, doc_id_col: str, text_col: str, n_gram_length: int=1) -> pd.DataFrame:
     # Preprocess
-    df['terms'] = df[text_col].apply(
-        lambda x: ' '.join(
+    df['tokens'] = df[text_col].apply(
+        lambda text: ' '.join(
             get_preprocessed_tokens(
-            x, 
-            n_gram_max_length=3, 
-            also_return_lower_case=False)))  # TODO: Test with True / False
+            text, 
+            n_gram_length=n_gram_length)))
     
     # Fit TF-IDF
     vectorizer = TfidfVectorizer(lowercase=False)
-    tfidf_matrix = vectorizer.fit_transform(df['terms'])
+    tfidf_matrix = vectorizer.fit_transform(df['tokens'])
     
     # Create a DataFrame from the sparse matrix
     tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out())
     tfidf_df['xkcd_id'] = df['xkcd_id']
     
     # Melt the DataFrame to long format and only keep tf-idf scores
-    title_text_tfidf_df = tfidf_df.melt(id_vars=doc_id_col, var_name='terms', value_name='tfidf')
+    title_text_tfidf_df = tfidf_df.melt(id_vars=doc_id_col, var_name='tokens', value_name='tfidf')
     title_text_tfidf_df = title_text_tfidf_df[title_text_tfidf_df['tfidf'] > 0]
+    
+    # Rename columns
+    title_text_tfidf_df = title_text_tfidf_df.rename(columns={'tokens': 'token'})
     return title_text_tfidf_df
 
-# %%
-# Get tfidf for titles
-tfidf_title_df = get_tfidf(xkcd_properties_df, 'xkcd_id', 'title')
-tfidf_title_df['heading'] = 'title'
+# %% GET DATA
+xkcd_properties_df = db_utils.get_xkcd_properties(db_path)
+xkcd_explanations_df = db_utils.get_xkcd_explained(db_path)
 
-# Get tfidf for title texts
-tfidf_title_text_df = get_tfidf(xkcd_properties_df, 'xkcd_id', 'title_text')
-tfidf_title_text_df['heading'] = 'title_text'
+# %% ANALYSE DATA
+for n_gram_length in tqdm(range(1, n_gram_max_length+1), desc='Calculating tf-idf scores...'):
+    # Get tfidf for titles
+    tfidf_title_df = get_tfidf(xkcd_properties_df, 'xkcd_id', 'title', n_gram_length=n_gram_length)
+    tfidf_title_df['heading'] = 'title'
 
-# Get tfidf for transcripts
-xkcd_transcripts_df = xkcd_explanations_df[xkcd_explanations_df['heading'] == 'Transcript']
-xkcd_transcripts_df = xkcd_transcripts_df.groupby(['xkcd_id', 'heading'], as_index=False).agg({
-    'text': lambda x: ' '.join(x)
-})
-tfidf_transcripts_df = get_tfidf(xkcd_transcripts_df, 'xkcd_id', 'text')
-tfidf_transcripts_df['heading'] = 'transcript'
+    # Get tfidf for title texts
+    tfidf_title_text_df = get_tfidf(xkcd_properties_df, 'xkcd_id', 'title_text', n_gram_length=n_gram_length)
+    tfidf_title_text_df['heading'] = 'title_text'
 
-# Get tfidf for rest of explanation, minus headings to ignore
-xkcd_rest_of_explanations_df = xkcd_explanations_df[~xkcd_explanations_df['heading'].isin(headings_ignore)]
-xkcd_rest_of_explanations_df = xkcd_rest_of_explanations_df.groupby(['xkcd_id', 'heading'], as_index=False).agg({
-    'text': lambda x: ' '.join(x)
-})
-tfidf_rest_of_explanations_df = get_tfidf(xkcd_rest_of_explanations_df, 'xkcd_id', 'text')
-tfidf_rest_of_explanations_df['heading'] = 'explanation'
+    # Get tfidf for transcripts
+    xkcd_transcripts_df = xkcd_explanations_df[xkcd_explanations_df['heading'] == 'Transcript']
+    xkcd_transcripts_df = xkcd_transcripts_df.groupby(['xkcd_id', 'heading'], as_index=False).agg({
+        'text': lambda x: ' '.join(x)
+    })
+    tfidf_transcripts_df = get_tfidf(xkcd_transcripts_df, 'xkcd_id', 'text', n_gram_length=n_gram_length)
+    tfidf_transcripts_df['heading'] = 'transcript'
 
-tfidf_df = pd.concat([
-    tfidf_title_df,
-    tfidf_title_text_df,
-    tfidf_transcripts_df,
-    tfidf_rest_of_explanations_df
-])
+    # Get tfidf for rest of explanation, minus headings to ignore
+    xkcd_rest_of_explanations_df = xkcd_explanations_df[~xkcd_explanations_df['heading'].isin(headings_ignore)]
+    xkcd_rest_of_explanations_df = xkcd_rest_of_explanations_df.groupby(['xkcd_id', 'heading'], as_index=False).agg({
+        'text': lambda x: ' '.join(x)
+    })
+    tfidf_rest_of_explanations_df = get_tfidf(xkcd_rest_of_explanations_df, 'xkcd_id', 'text', n_gram_length=n_gram_length)
 
-tfidf_df
-# %%
-# Add tf-idf scores to the database
-
+    tfidf_df = pd.concat([
+        tfidf_title_df,
+        tfidf_title_text_df,
+        tfidf_transcripts_df,
+        tfidf_rest_of_explanations_df
+    ])
+    
+    # Add tf-idf scores to the database
+    db_utils.insert_tfidfs_into_db(db_path, tfidf_df)
+    
+# %% CHECK OUT THE RESULTS
+# TODO: Check out some results
+# Number of tf_idfs per heading
+# Are all xkcd with tfidfs
